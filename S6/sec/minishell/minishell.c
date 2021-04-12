34d8e1dce1d56c0b03638dd8c6ce7d2cc3859cdb
NextFire <git@yuru.moe>
2021-04-12 20:30:43 +0200

    minishell base ok

--- S6/sec/minishell/minishell.c
+++ S6/sec/minishell/minishell.c
@@ -1,12 +1,32 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
 #include "readcmd.h"
 
 int main(int argc, char const *argv[])
 {
     struct cmdline *cmd;
+    pid_t status;
+
     while (1)
     {
         printf(">>> ");
         cmd = readcmd();
+
+        switch (fork())
+        {
+        case -1:
+            printf("ECHEC fork\n");
+            break;
+        case 0:
+            execvp(cmd->seq[0][0], cmd->seq[0]);
+            printf("ECHEC exec\n");
+            break;
+        default:
+            wait(&status);
+            break;
+        }
     }
+
+    return 0;
 }
