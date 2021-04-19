777bda005b1e25041aabe7769c9dfdc7a46998c5
NextFire <git@yuru.moe>
2021-04-19 18:18:11 +0200

    q1-5

---
+++ S6/sec/minishell/Q3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <errno.h>
+#include "readcmd.h"
+
+int main(int argc, char const *argv[])
+{
+    struct cmdline *cmd;
+    pid_t pid_fils;
+    while (1)
+    {
+        printf("$ ");
+        cmd = readcmd();
+
+        switch (pid_fils = fork())
+        {
+        case -1:
+            perror("fork");
+            break;
+
+        case 0:
+            execvp(cmd->seq[0][0], cmd->seq[0]);
+            printf("%s\n", cmd->err);
+            exit(getpid());
+            break;
+
+        default:
+        {
+            wait(NULL);
+            break;
+        }
+        }
+    }
+
+    return 1; // Pas normal si on se retrouve ici...
+}
