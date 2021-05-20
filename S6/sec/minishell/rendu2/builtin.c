47082c03032e2bca8f1140c3b71f02cd3a6e072b
NextFire <git@yuru.moe>
2021-05-20 16:06:57 +0200

    update

---
+++ S6/sec/minishell/rendu2/builtin.c
@@ -0,0 +1,78 @@
+
+#include "builtin.h"
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <signal.h>
+#include "process.h"
+
+void jobs(struct process **pl, char **cmd)
+{
+    struct process *curseur = *pl;
+    puts("ID\tPID\tSTATE\tCOMMAND");
+    while (curseur != NULL)
+    {
+        printf("%d\t%d\t%s\t%s\n", curseur->id, curseur->pid, (curseur->is_running) ? "Running" : "Stopped", curseur->cmd);
+        curseur = curseur->prec;
+    }
+}
+
+void stop(struct process **pl, char **cmd)
+{
+    struct process **p_to_stop = (cmd[1]) ? pl_get_id(pl, atoi(cmd[1])) : pl;
+    kill((*p_to_stop)->pid, SIGSTOP);
+}
+
+void cont(struct process **pl, char **cmd)
+{
+    struct process **p_to_bg = (cmd[1]) ? pl_get_id(pl, atoi(cmd[1])) : pl;
+    kill((*p_to_bg)->pid, SIGCONT);
+    (*p_to_bg)->is_running = RUNNING;
+    printf("[%d] %d: %s\n", (*p_to_bg)->id, (*p_to_bg)->pid, (*p_to_bg)->cmd);
+}
+
+void bg(struct process **pl, char **cmd)
+{
+    cont(pl, cmd);
+}
+
+void fg(struct process **pl, char **cmd)
+{
+    cont(pl, cmd);
+    pause();
+}
+
+int builtin(struct process **pl, char **cmd)
+{
+    int is_builtin = 1;
+    if (strcmp(cmd[0], "cd") == 0)
+    {
+        chdir(cmd[1]);
+    }
+    else if (strcmp(cmd[0], "exit") == 0)
+    {
+        exit(EXIT_SUCCESS);
+    }
+    else if (strcmp(cmd[0], "jobs") == 0)
+    {
+        jobs(pl, cmd);
+    }
+    else if (strcmp(cmd[0], "stop") == 0)
+    {
+        stop(pl, cmd);
+    }
+    else if (strcmp(cmd[0], "bg") == 0)
+    {
+        bg(pl, cmd);
+    }
+    else if (strcmp(cmd[0], "fg") == 0)
+    {
+        fg(pl, cmd);
+    }
+    else
+    {
+        is_builtin = 0;
+    }
+    return is_builtin;
+}
---
+++ S6/sec/minishell/rendu2/process.c
@@ -0,0 +1,67 @@
+#include "process.h"
+#include <stdlib.h>
+#include <string.h>
+
+int id = 1;
+
+char *cmd_to_string(char **cmd)
+{
+    char *string = malloc((strlen(cmd[0]) + 1) * sizeof(char));
+    strcpy(string, cmd[0]);
+    for (int i = 1; cmd[i]; i++)
+    {
+        string = realloc(string, (strlen(cmd[i]) + 2) * sizeof(char));
+        strcat(strcat(string, " "), cmd[i]);
+    }
+    return string;
+}
+
+int pl_add(struct process **pl, pid_t pid, char **cmd)
+{
+    struct process *p = malloc(sizeof(struct process));
+    p->id = id++;
+    p->pid = pid;
+    p->is_running = RUNNING;
+    p->cmd = cmd_to_string(cmd);
+    p->prec = *pl;
+    *pl = p;
+    return p->id;
+}
+
+void pl_remove(struct process **pl, pid_t pid)
+{
+    if ((*pl)->pid == pid) // celui à retirer est en tête
+    {
+        struct process *prec = (*pl)->prec;
+        free(*pl);
+        *pl = prec;
+    }
+    else // il est qque part au milieu
+    {
+        if ((*pl)->prec->pid != pid)
+        {
+            pl_remove(&(*pl)->prec, pid);
+        }
+        struct process *to_free = (*pl)->prec;
+        (*pl)->prec = to_free->prec;
+        free(to_free);
+    }
+}
+
+struct process **pl_get_id(struct process **pl, int id)
+{
+    if ((*pl)->id == id)
+    {
+        return pl;
+    }
+    return pl_get_id(&(*pl)->prec, id);
+}
+
+struct process **pl_get_pid(struct process **pl, pid_t pid)
+{
+    if ((*pl)->pid == pid)
+    {
+        return pl;
+    }
+    return pl_get_pid(&(*pl)->prec, pid);
+}
